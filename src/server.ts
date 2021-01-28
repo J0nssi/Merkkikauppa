7a4e5a01c7bc4c6b2068e872e40ddc3e4e9e3802
Erik Dubois <lassi_koykka@hotmail.com>
2021-01-28 15:53:13 +0200

    Node.js/Express ja react pohja luotu sivustolle

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Moikkomoi.')
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
-const PORT = process.env.PORT || 5000
+
+const PORT = process.env.PORT || 5000;
 
 app.listen( PORT, () => console.log(`Server started on port ${PORT}`))
