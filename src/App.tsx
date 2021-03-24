3bdd2b1d4bf06c45a75ef1265c87920686cffd99
Joonas <joonas.vaija@edu.savonia.fi>
2021-03-24 10:06:21 +0200

    Kirjautumis- ja rekisteröintisivun tyylittely, profiilisivun raakile

--- src/App.tsx
+++ src/App.tsx
@@ -4,47 +4,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { IListing } from '../server/models/listingModel'
 import Login from './components/Login/Login';
 import Registration from './components/Registration/Registration';
+import Profilepage from './components/Profilepage/Profilepage';
 
 function App() {
-  const [listings, setListings] = useState<Array<IListing>>([])
 
-  useEffect(() => {
-    const getListings = async () => {
-      return await axios.get(`/listings`)
-        .then(response => {
-          console.log(response.data)
-          setListings(response.data);
-        })
-        .catch(err => console.log(err));
-    }
 
-    getListings();
-  }, [])
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/kirjaudu">
-          <Login />
-        </Route>
-        <Route path="/rekisteroidy">
-          <Registration />
-        </Route>
-        <Route path="/">
-          {listings.map(listing => {
-            return (
-              <div style={{ width: "min-content", margin: "auto" }}>
-                {listing.urls.map(url => { return <img src={url} alt="" /> })}
-                <h3>
-                  {listing.title} <br /> {listing.price} €
-            </h3>
-                <h4>{listing.seller.name}</h4>
-              </div>
-            )
-          })}
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Profilepage></Profilepage>
+      <Registration></Registration>
+      <Login></Login>
+    </>
   );
 }
 
--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,10 +1,59 @@
 import axios from "axios";
 import { ChangeEvent, MouseEvent, useState } from "react";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
 
 axios.defaults.withCredentials = true;
 
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="">
+          Merkkikauppa
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 const Registration = () => {
+    const classes = useStyles();
     const [state, setState] = useState({
         email: "",
         password: "",
@@ -51,6 +100,100 @@ const Registration = () => {
                 <input type="city" name="city" id="city" placeholder="Kaupunki" onChange={handleChange} required />
                 <button type="submit" onClick={handleSubmitClick}>Luo käyttäjä</button>
             </form>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Rekisteröidy
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="name"
+                                    label="Nimi"
+                                    name="name"
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Sähköposti"
+                                    name="email"
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Salasana"
+                                    type="password"
+                                    id="password"
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="city"
+                                    label="Kaupunki"
+                                    name="city"
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="phone_number"
+                                    label="Puhelinnumero"
+                                    name="phone_number"
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={handleSubmitClick}
+                        >
+                            Rekisteröidy
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    Onko sinulla jo käyttäjä? Kirjaudu sisään
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+                <Box mt={5}>
+                    <Copyright />
+                </Box>
+            </Container>
+  );
+
         </>
     )
 }
