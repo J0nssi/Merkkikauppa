48367be5ea4d622ec5956884de2f7451026dd41a
Joonas <joonas.vaija@edu.savonia.fi>
2021-04-27 16:37:18 +0300

    Myynti-ilmoituksen lisäyssivun tyylittely

--- src/components/AddListing/AddListing.tsx
+++ src/components/AddListing/AddListing.tsx
@@ -2,11 +2,44 @@ import axios from "axios";
 import { useState, MouseEvent, ChangeEvent, useContext } from "react";
 import { Redirect } from "react-router";
 import { userContext } from "../../userContext";
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 
 
 axios.defaults.withCredentials = true;
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
 const AddListing = () => {
+
+    const classes = useStyles();
 
     const [urlState, setUrlState] = useState("");
 
@@ -86,7 +119,7 @@ const AddListing = () => {
     
     return (
         <>
-            <form style={{display: "flex", flexDirection: "column", maxWidth: "500px", margin: "0 auto"}}>
+            {/*<form style={{display: "flex", flexDirection: "column", maxWidth: "500px", margin: "0 auto"}}>
                 <h1>Lisää myynti-ilmoitus</h1>
                 <form>
                     <ul id="url_list">
@@ -101,8 +134,105 @@ const AddListing = () => {
                 <textarea placeholder="Kuvaus" id="description" rows={3} onChange={handleChange} />
                 <input placeholder="Hinta" id="price" type="number" min="0" onChange={handleChange} style={{width: "30%"}}/>
                 <input placeholder="Määrä" id="item_count" type="number" min="1" onChange={handleChange} style={{width: "30%"}}/>
-                <button type="submit" onClick={handleSubmitClick}>Lisää</button>
-            </form>
+    <button type="submit" onClick={handleSubmitClick}>Lisää</button>*/}
+
+                <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                        Lisää myynti-ilmoitus
+                    </Typography>
+                    <form className={classes.form} noValidate> 
+                    <ul id="url_list">
+                        {state.urls.map(url => { return (<img src={url} alt="pic_placeholder" style={{width: "200px"}}/>) })}
+                    </ul>
+                    <input placeholder="Kuvan url" type="url" id="image_url" onChange={handleUrlChange} style={{width: "100%"}}/>
+                    <Button
+                            type="submit"
+                            id="btnAddUrl"
+                            disabled={urlState.length <= 0}
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={handleUrlSubmit}
+                        >
+                            Lisää kuvan url
+                        </Button>
+
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="title"
+                                    label="Merkin nimi"
+                                    type="text"
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    multiline
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="description"
+                                    label="Kuvaus merkistä"
+                                    rows="3"                               
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="price"
+                                    label="Hinta"
+                                    type="number"
+                                    InputProps={{
+                                        inputProps: { 
+                                            max: 999, min: 0
+                                        }
+                                    }}
+                                    style={{width: "50%"}}
+                                    onChange={handleChange}
+                                />
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="item_count"
+                                    label="Määrä"
+                                    type="number"
+                                    InputProps={{
+                                        inputProps: { 
+                                            max: 999, min: 1
+                                        }
+                                    }}
+                                    style={{width: "50%"}}
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={handleSubmitClick}
+                        >
+                            Lisää
+                        </Button>
+                    </form>
+                </div>
+
+            </Container>
+                
         </>
     )
 }
